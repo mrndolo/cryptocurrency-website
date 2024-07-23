16300e42575a781d985e9ec38b534852ae86fbcf
mrndolo <callistomutunga@gmail.com>
2024-07-23 09:18:27 +0300

    updated the live price on the website elements

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("ethereum");
+var doge = document.getElementById("dogecoin");
 
 var settings= {
     "async": true,
@@ -8,5 +10,8 @@ var settings= {
     "headers": {}
 }
 $.ajax(settings).done(function (response){
-    console.log(response);
+    // console.log(response);
+    btc.innerHTML = response.bitcoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
 })
